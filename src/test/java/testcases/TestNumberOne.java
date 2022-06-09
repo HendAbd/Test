fdcf67f1f829b2cca58fc362c6dad340817470a5
HendAbd <106052935+HendAbd@users.noreply.github.com>
2022-06-09 10:16:44 -0600

    gfg

--- src/test/java/testcases/TestNumberOne.java
+++ src/test/java/testcases/TestNumberOne.java
@@ -44,10 +44,10 @@ public class TestNumberOne {
         homePage = new BrowserStackHomePage(driver);
         homePage.clickHeader();
         RunReports.takeScreenShot(driver,System.getProperty("user.dir") +"/test-output/img1.jpg");
-        RunReports.test.addScreenCaptureFromPath(System.getProperty("user.dir") +"/test-output/img1.png");
+        RunReports.test.addScreenCaptureFromPath("img1.png");
         homePage.clickSignButton();
         RunReports.takeScreenShot(driver,System.getProperty("user.dir") +"/test-output/img2.jpg");
-        RunReports.test.addScreenCaptureFromPath(System.getProperty("user.dir") +"/test-output/img2.png");
+        RunReports.test.addScreenCaptureFromPath("img2.png");
     }
     @Test(priority = 2)
     public void enter_userDetails() throws InterruptedException, IOException {
@@ -57,7 +57,7 @@ public class TestNumberOne {
         signPage.businessEmailClick("TestUser@gmail.com");
         signPage.passwordClick("TestUserPassword");
         RunReports.takeScreenShot(driver,System.getProperty("user.dir") +"/test-output/img3.jpg");
-        RunReports.test.addScreenCaptureFromPath(System.getProperty("user.dir") +"/test-output/img3.png");
+        RunReports.test.addScreenCaptureFromPath("img3.png");
 
 
         Thread.sleep(10000);
